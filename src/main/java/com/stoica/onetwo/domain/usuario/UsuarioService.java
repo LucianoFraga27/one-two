6a3aa69c9624768616a88d90608c4b99c98b9e6c
LcnFraga27 <luciano.lucas@bllc.com.br>
2023-11-01 03:20:29 -0300

    criando controller de usuario

--- src/main/java/com/stoica/onetwo/domain/usuario/UsuarioService.java
+++ src/main/java/com/stoica/onetwo/domain/usuario/UsuarioService.java
@@ -1,6 +1,13 @@
 package com.stoica.onetwo.domain.usuario;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UsuarioService {
+	
+	List<UsuarioModel> findAll();
+	UsuarioModel findById(Long id);
+	
 	
 	UsuarioModel save (UsuarioModel usuario);
 	
--- src/main/java/com/stoica/onetwo/domain/usuario/persistence/UsuarioServiceImpl.java
+++ src/main/java/com/stoica/onetwo/domain/usuario/persistence/UsuarioServiceImpl.java
@@ -1,5 +1,8 @@
 package com.stoica.onetwo.domain.usuario.persistence;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.stoica.onetwo.domain.usuario.UsuarioModel;
@@ -14,9 +17,21 @@ public class UsuarioServiceImpl implements UsuarioService{
 	UsuarioRepository usuarioRepository;
 
 	@Override
+	public List<UsuarioModel> findAll() {
+		return usuarioRepository.findAll();
+	}
+
+	@Override
+	public UsuarioModel findById(Long id) {
+		return usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Erro ao encontrar usuario com o Id "+id));
+	}
+	
+	@Override
 	public UsuarioModel save(UsuarioModel usuario) {
 		return usuarioRepository.save(usuario);
 	}
+
+	
 	
 	
 }
