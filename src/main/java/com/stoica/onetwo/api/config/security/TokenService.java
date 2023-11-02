41913eef64cc9be80715ce448225e09a7d7e7c65
LcnFraga27 <luciano.lucas@bllc.com.br>
2023-11-02 01:21:57 -0300

    melhorando token

--- src/main/java/com/stoica/onetwo/api/config/security/TokenService.java
+++ src/main/java/com/stoica/onetwo/api/config/security/TokenService.java
@@ -6,6 +6,7 @@ import java.time.ZoneOffset;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,10 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.stoica.onetwo.api.resource.usuario.dto.UsuarioMapper;
 import com.stoica.onetwo.domain.auth.AuthModel;
+import com.stoica.onetwo.domain.usuario.UsuarioModel;
+import com.stoica.onetwo.domain.usuario.UsuarioService;
 
 @Service
 public class TokenService {
@@ -21,18 +25,38 @@ public class TokenService {
 	@Value("${api.security.token.secret}")
 	private String secret ;
 	
+	@Autowired
+	UsuarioService usuarioService;
+	
+	@Autowired
+	UsuarioMapper usuarioMapper;
+	
 	public Map<String, String> generateToken(AuthModel user) {
 		try {
 		    
 			Algorithm algorithm = Algorithm.HMAC256(secret);
-		    String token = JWT.create()
+		    
+			UsuarioModel usuarioModel = usuarioMapper.findById(user.getId());
+			
+			Map responseToken = new HashMap<>();
+			
+			responseToken.put("id", usuarioModel.getId());
+			responseToken.put("email", usuarioModel.getEmail());
+			responseToken.put("username", usuarioModel.getUsername());
+			responseToken.put("foto", usuarioModel.getFotoPerfil());
+			
+			String token = JWT.create()
 		        .withIssuer("one-two")
 		        .withSubject(user.getLogin())
 		        .withExpiresAt(expiresIn())
+		        .withClaim("user", responseToken)
 		        .sign(algorithm);
-		    Map<String, String> response = new HashMap<>();
+		    
+		    Map response = new HashMap<>();
 		    response.put("access_token", token);
-		    response.put("id", user.getId().toString());
+		    response.put("expires_in", 18000);
+		    response.put("token_type", "Bearer");
+		    // response.put("refresh_token", null);
 		    return response;
 		} catch (JWTCreationException exception){
 		    throw new RuntimeException("Erro ao criar JWT TOKEN");
