ad2df7292fa8944aeae1bb07c52ed2c7d3a38500
LcnFraga27 <luciano.lucas@bllc.com.br>
2023-11-02 15:22:37 -0300

    cors config

--- src/main/java/com/stoica/onetwo/api/config/cors/CorsConfig.java
+++ src/main/java/com/stoica/onetwo/api/config/cors/CorsConfig.java
@@ -1,5 +1,16 @@
 package com.stoica.onetwo.api.config.cors;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("Origin","X-Requested-With", "Content-Type", "Accept", "X-CustomToken", "Authorization", "sentry-trace", "baggage")
+                .allowedOrigins("http://localhost:59157")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
 }
