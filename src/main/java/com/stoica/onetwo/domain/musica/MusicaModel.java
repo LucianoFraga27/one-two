b2778172e27dcdb16c1322411aa9bf8c8c34781e
Luciano Morais <lcnmorais@gmail.com>
2023-11-06 23:46:33 -0300

    mapeando curtidas

--- src/main/java/com/stoica/onetwo/domain/musica/MusicaModel.java
+++ src/main/java/com/stoica/onetwo/domain/musica/MusicaModel.java
@@ -10,28 +10,35 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Entity(name="musica")
+@Entity(name = "musica")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class MusicaModel {
-	
-	@EqualsAndHashCode.Include
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private String titulo;
-	
-	private String capa;
-	
-	private String audio;
-	
-	private GeneroEnum genero;
-	
-	private Long usuario;
-	
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String capa;
+    private String audio;
+    private GeneroEnum genero;
+
+    @ManyToOne // Um usuário pode curtir várias músicas
+    @JoinColumn(name = "usuario_id") // Coluna que faz a ligação entre MusicaModel e UsuarioModel
+    private UsuarioModel usuario;
+
+    @ManyToMany
+    @JoinTable(name = "curtidas",
+            joinColumns = @JoinColumn(name = "musica_id"),
+            inverseJoinColumns = @JoinColumn(name = "usuario_id"))
+    private List<UsuarioModel> usuariosCurtiram;
 }
