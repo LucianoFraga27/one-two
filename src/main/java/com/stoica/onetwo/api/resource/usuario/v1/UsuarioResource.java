57164e2f0b18af12e1fee7817c942c38c82dc6f8
Luciano Morais <lcnmorais@gmail.com>
2023-11-11 15:14:07 -0300

    usuario resource

--- src/main/java/com/stoica/onetwo/api/resource/usuario/v1/UsuarioResource.java
+++ src/main/java/com/stoica/onetwo/api/resource/usuario/v1/UsuarioResource.java
@@ -1,19 +1,25 @@
 package com.stoica.onetwo.api.resource.usuario.v1;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stoica.onetwo.api.resource.usuario.dto.AddUsernameAndFotoDTO;
+import com.stoica.onetwo.api.resource.usuario.dto.GetUsuarioResponseDTO;
 import com.stoica.onetwo.api.resource.usuario.dto.UsuarioMapper;
 import com.stoica.onetwo.domain.usuario.UsuarioModel;
 import com.stoica.onetwo.domain.usuario.UsuarioService;
+import com.stoica.onetwo.domain.usuario.seguidores.RelacionamentoSeguidorService;
 
 import lombok.AllArgsConstructor;
 
@@ -22,22 +28,90 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class UsuarioResource {
 
-	UsuarioService usuarioService;
-	UsuarioMapper usuarioMapper;
-	
+	private UsuarioService usuarioService;
+	private UsuarioMapper usuarioMapper;
+	private RelacionamentoSeguidorService relacionamentoSeguidorService;
+
+
 	@GetMapping
-	public List<UsuarioModel> findAll() {
+	public List<GetUsuarioResponseDTO> findAll() {
 		return usuarioMapper.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
-	public UsuarioModel findById(@PathVariable(value="id") Long id) {
+	public GetUsuarioResponseDTO findById(@PathVariable(value = "id") Long id) {
 		return usuarioMapper.findById(id);
 	}
-	
-	@PostMapping(value = "/{id}",consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
-	public void addUsernameAndFoto (@PathVariable(value="id") Long id, AddUsernameAndFotoDTO post) throws IOException {
-		usuarioService.addUsernameAndFoto(id,post, post.fotoPerfil().getInputStream());
+
+	@PostMapping(value = "/{id}", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+	public void addUsernameAndFoto(@PathVariable(value = "id") Long id, AddUsernameAndFotoDTO post) throws IOException {
+		usuarioService.addUsernameAndFoto(id, post, post.fotoPerfil().getInputStream());
 	}
-	
+
+	@PostMapping("/{seguidorId}/seguir/{seguidoId}")
+    public ResponseEntity<String> seguirUsuario(@PathVariable Long seguidorId, @PathVariable Long seguidoId) {
+        // Busque os usuários do banco de dados (você pode fazer isso com seu serviço de usuário)
+        UsuarioModel seguidor = usuarioService.findById(seguidorId);
+        UsuarioModel seguido = usuarioService.findById(seguidoId);
+
+        relacionamentoSeguidorService.seguirUsuario(seguidor, seguido);
+
+        return ResponseEntity.ok("Usuário seguido com sucesso.");
+    }
+
+    @PostMapping("/{seguidorId}/deixar-de-seguir/{seguidoId}")
+    public ResponseEntity<String> deixarDeSeguirUsuario(@PathVariable Long seguidorId, @PathVariable Long seguidoId) {
+        // Busque os usuários do banco de dados (você pode fazer isso com seu serviço de usuário)
+        UsuarioModel seguidor = usuarioService.findById(seguidorId);
+        UsuarioModel seguido = usuarioService.findById(seguidoId);
+
+        relacionamentoSeguidorService.deixarDeSeguirUsuario(seguidor, seguido);
+
+        return ResponseEntity.ok("Usuário deixou de ser seguido com sucesso.");
+    }
+
+    @GetMapping("/{usuarioId}/seguidores")
+    public ResponseEntity<List<UsuarioModel>> listarSeguidores(@PathVariable Long usuarioId) {
+        // Busque o usuário do banco de dados (você pode fazer isso com seu serviço de usuário)
+        UsuarioModel usuario = usuarioService.findById(usuarioId);
+
+        // Chame o serviço para listar os seguidores do usuário
+        List<UsuarioModel> seguidores = relacionamentoSeguidorService.listarSeguidores(usuario);
+
+        return ResponseEntity.ok(seguidores);
+    }
+
+    @GetMapping("/{usuarioId}/seguindo")
+    public ResponseEntity<List<UsuarioModel>> listarSeguindo(@PathVariable Long usuarioId) {
+        // Busque o usuário do banco de dados (você pode fazer isso com seu serviço de usuário)
+        UsuarioModel usuario = usuarioService.findById(usuarioId);
+
+        // Chame o serviço para listar quem o usuário está seguindo
+        List<UsuarioModel> seguindo = relacionamentoSeguidorService.listarSeguindo(usuario);
+
+        return ResponseEntity.ok(seguindo);
+    }
+
+	@PostMapping("/{seguidorId}/seguir2/{seguidoId}")
+	public ResponseEntity<String> seguirOuDeixarDeSeguirUsuario(@PathVariable Long seguidorId, @PathVariable Long seguidoId) {
+    // Busque os usuários do banco de dados (você pode fazer isso com seu serviço de usuário)
+    UsuarioModel seguidor = usuarioService.findById(seguidorId);
+    UsuarioModel seguido = usuarioService.findById(seguidoId);
+
+    // Verifica se o usuário já é seguido
+    boolean jaSegue = relacionamentoSeguidorService.verificarSeUsuarioSegue(seguidor, seguido);
+
+    if (jaSegue) {
+        // Se já está seguindo, deixe de seguir
+        relacionamentoSeguidorService.deixarDeSeguirUsuario(seguidor, seguido);
+        return ResponseEntity.ok("Usuário deixou de ser seguido com sucesso.");
+    } else {
+        // Se não está seguindo, siga
+        relacionamentoSeguidorService.seguirUsuario(seguidor, seguido);
+        return ResponseEntity.ok("Usuário seguido com sucesso.");
+    }
+}
+
+
+
 }
